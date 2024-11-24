a9aada30a7b39f7f496da6bae1966920c56bd3a6
Macan Mehri <macanmehri@gmail.com>
2024-11-24 16:49:00 +0330

    Started project and added signup login to it

---
+++ ecommerce/sample_settings.py
@@ -0,0 +1,19 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+# Database information
+DATABASE = {
+    'NAME': '',
+    'USER': '',
+    'PASSWORD': '',
+    'HOST': '',
+    'PORT': 0,
+}
+
+# Admin path in url
+ADMIN_PATH = ''
---
+++ ecommerce/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from .local_settings import ADMIN_PATH
+
+urlpatterns = [
+    path(f'{ADMIN_PATH}', admin.site.urls),
+    path('users/', include('users.urls'))
+]
