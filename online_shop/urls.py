948abec4c7b6507d132e066141d3475aef3adcae
Macan Mehri <macanmehri@gmail.com>
2024-11-24 22:37:00 +0330

    Created views for products and categories

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from .views import category_view, product_view
+
 
 urlpatterns = [
-
+    path('categories/', category_view, name='categories'),
+    path('products/<int:category_id>', product_view, name='products'),
 ]
--- online_shop/views.py
+++ online_shop/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product, Category
 
-# Create your views here.
+
+def category_view(request):
+    categories = Category.objects.filter(is_active=True)
+
+    return render(request, 'categories.html', {'categories': categories})
+
+
+def product_view(request, category_id):
+    products = Product.objects.filter(is_active=True, category__id=category_id)
+
+    return render(request, 'products.html', {'products': products})
+
