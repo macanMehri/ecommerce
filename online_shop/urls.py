215e6fa74449c28fdf1c4f35c1ce1dfebc62be30
Macan Mehri <macanmehri@gmail.com>
2024-11-25 11:27:18 +0330

    Added logout button in categories page

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 from .views import category_view, product_view
 
 
 urlpatterns = [
     path('categories/', category_view, name='categories'),
     path('products/<int:category_id>', product_view, name='products'),
+    path('logout/', LogoutView.as_view(next_page='/online_shop/categories/'), name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -12,7 +12,7 @@ def login_view(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home')
+                return redirect('categories')
         else:
             form.add_error(None, 'Invalid username or password! Please try again.')
     else:
