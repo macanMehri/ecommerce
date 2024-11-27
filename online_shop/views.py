ef6ef0f10690e151bc7181110a83cb3e5817b785
Macan Mehri <macanmehri@gmail.com>
2024-11-27 11:20:08 +0330

    Ordered the items

--- online_shop/views.py
+++ online_shop/views.py
@@ -18,13 +18,13 @@ def calculate_total(purchases) -> float:
 
 
 def category_view(request):
-    categories = Category.objects.filter(is_active=True)
+    categories = Category.objects.filter(is_active=True).order_by('title')
 
     return render(request, 'categories.html', {'categories': categories})
 
 
 def product_view(request, category_id):
-    products = Product.objects.filter(is_active=True, category__id=category_id)
+    products = Product.objects.filter(is_active=True, category__id=category_id).order_by('-created_date')
 
     return render(request, 'products.html', {'products': products})
 
@@ -32,7 +32,7 @@ def product_view(request, category_id):
 @login_required
 def insurances_view(request):
     if request.user.is_staff:
-        insurances = Insurance.objects.filter(is_active=True)
+        insurances = Insurance.objects.filter(is_active=True).order_by('name')
         return render(request, 'insurances.html', {'insurances': insurances})
     else:
         redirect('categories')
@@ -141,7 +141,9 @@ def delete_product_view(request, product_id):
 
 @login_required
 def purchase_basket_view(request):
-    purchases_to_do = PurchaseBasket.objects.filter(user=request.user, is_completed=False)
+    purchases_to_do = PurchaseBasket.objects.filter(
+        user=request.user, is_completed=False,
+    ).order_by('-created_date')
 
     total = calculate_total(purchases=purchases_to_do)
 
@@ -154,7 +156,9 @@ def purchase_basket_view(request):
 
 @login_required
 def purchase_history_view(request):
-    purchases_history = PurchaseBasket.objects.filter(user=request.user, is_completed=True)
+    purchases_history = PurchaseBasket.objects.filter(
+        user=request.user, is_completed=True
+    ).order_by('created_date')
 
     total = calculate_total(purchases=purchases_history)
 
