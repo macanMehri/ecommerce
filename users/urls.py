090f5f401ac66f159876333f0307aa7d076c86ec
Macan Mehri <macanmehri@gmail.com>
2024-12-01 15:05:53 +0330

    asigned address to user

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import login_view, signup_view, add_address_view
+from .views import login_view, signup_view, add_address_view, get_cities, my_addresses_view
 
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('signup/', signup_view, name='signup'),
     path('add_address/', add_address_view, name='add_address'),
+    path('get_cities/<int:province_id>/', get_cities, name='get_cities'),
+    path('my_addresses/', my_addresses_view, name='my_addresses'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import LoginForm, SignUpForm, AddressForm
 from django.contrib.auth import authenticate, login
 from online_shop import templates
@@ -41,21 +41,34 @@ def signup_view(request):
 
 
 @login_required
+def get_cities(request, province_id):
+    cities = ProvinceCities.objects.filter(province_id=province_id).select_related('city')
+    city_data = [{'id': city.city.id, 'name': city.city.name} for city in cities]
+    return JsonResponse({'cities': city_data})
+
+
+@login_required
 def add_address_view(request):
+    provinces = Province.objects.all()
     if request.method == 'POST':
         form = AddressForm(request.POST)
         if form.is_valid():
             address = form.save(commit=False)
             address.save()
-            user_address, _ = UserAddress.objects.get_or_create(
+            UserAddress.objects.get_or_create(
                 user=request.user,
                 address=address,
                 defaults={'is_default': False},
             )
             # return redirect('')
-
     else:
         form = AddressForm()
 
-    return render(request, 'add_address.html', {'form': form})
+    return render(request, 'add_address.html', {'form': form, 'provinces': provinces})
 
+
+@login_required
+def my_addresses_view(request):
+    addresses = Address.objects.filter(
+        id__in=UserAddress.objects.filter(user=request.user).values_list('address', flat=True))
+    return render(request, 'my_addresses.html', {'addresses': addresses})
