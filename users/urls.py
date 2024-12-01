fc0bf0e22f61ede65c4b67715feb2a6814b7be98
Macan Mehri <macanmehri@gmail.com>
2024-12-01 14:15:14 +0330

    Removed a bug and added email field to signup page

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-from .views import login_view, signup_view, get_cities_by_province, add_address_view
+from .views import login_view, signup_view, add_address_view
 
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('signup/', signup_view, name='signup'),
-    path(
-        'get_cities_by_province/<int:province_id>/',
-        get_cities_by_province,
-        name='get_cities_by_province'),
     path('add_address/', add_address_view, name='add_address'),
 ]
